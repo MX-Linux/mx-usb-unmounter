53525cf5b67f7c3d26471c994aeac6e43035af54
oldgizahub <geoffgigg@hotmail.com>
2016-11-30 11:13:04 -0500

    new French translations for mx-usb-unmounter

--- translations/mx-usb-unmounter_fr.ts
+++ translations/mx-usb-unmounter_fr.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
 <context>
     <name>usbunmounter</name>
     <message>
@@ -9,7 +7,7 @@
         <location filename="../usbunmounter.cpp" line="317"/>
         <location filename="../usbunmounter.cpp" line="324"/>
         <source>MX USB Unmounter</source>
-        <translation>MX Démonte-USB</translation>
+        <translation>Outil MX Démonte-USB</translation>
     </message>
     <message>
         <location filename="../usbunmounter.ui" line="51"/>
@@ -29,17 +27,17 @@
     <message>
         <location filename="../usbunmounter.cpp" line="205"/>
         <source>Unmounting </source>
-        <translation>On démonte</translation>
+        <translation>Démontage</translation>
     </message>
     <message>
         <location filename="../usbunmounter.cpp" line="207"/>
         <source>Other partitions still mounted on device</source>
-        <translation>Autres partitions encore montées sur l&apos;unité</translation>
+        <translation>Autres partitions encore montées sur le périphérique</translation>
     </message>
     <message>
         <location filename="../usbunmounter.cpp" line="276"/>
         <source>Unable to  Unmount, Device in Use</source>
-        <translation>On ne peut pas démonter, l&apos;unité toujours utilisée</translation>
+        <translation>Impossible de démonter, le périphérique est en cours d&apos;utilisation</translation>
     </message>
     <message>
         <location filename="../usbunmounter.cpp" line="316"/>
