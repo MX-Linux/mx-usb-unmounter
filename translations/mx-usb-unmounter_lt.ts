79c506ec2dddd5d06f625a2afc710208fe72c131
oldgizahub <geoffgigg@hotmail.com>
2017-12-04 09:57:36 -0500

    updated desktop and lt translation for mx-usb-unmounter

--- translations/mx-usb-unmounter_lt.ts
+++ translations/mx-usb-unmounter_lt.ts
@@ -1,15 +1,13 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="lt">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
 <context>
     <name>usbunmounter</name>
     <message>
         <location filename="../usbunmounter.ui" line="27"/>
         <location filename="../usbunmounter.cpp" line="208"/>
-        <location filename="../usbunmounter.cpp" line="327"/>
-        <location filename="../usbunmounter.cpp" line="334"/>
+        <location filename="../usbunmounter.cpp" line="317"/>
+        <location filename="../usbunmounter.cpp" line="324"/>
         <source>MX USB Unmounter</source>
-        <translation type="unfinished"></translation>
+        <translation>MX USB atjungiklis</translation>
     </message>
     <message>
         <location filename="../usbunmounter.ui" line="51"/>
@@ -37,38 +35,38 @@
         <translation>Kiti, vis dar įrenginyje prijungti skaidiniai</translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="286"/>
+        <location filename="../usbunmounter.cpp" line="276"/>
         <source>Unable to  Unmount, Device in Use</source>
         <translation>Nepavyko atjungti, įrenginys yra naudojamas</translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="326"/>
+        <location filename="../usbunmounter.cpp" line="316"/>
         <source>About MX USB Unmounter</source>
-        <translation type="unfinished"></translation>
+        <translation>Apie MX USB atjungiklis</translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="327"/>
+        <location filename="../usbunmounter.cpp" line="317"/>
         <source>Version: </source>
         <translation>Versija: </translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="328"/>
+        <location filename="../usbunmounter.cpp" line="318"/>
         <source>Quickly Unmount Removable Media</source>
         <translation>Greitai atjungti keičiamąsias laikmenas</translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="330"/>
+        <location filename="../usbunmounter.cpp" line="320"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Autorių teisės (c) MX Linux</translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="331"/>
+        <location filename="../usbunmounter.cpp" line="321"/>
         <source>Cancel</source>
         <translation>Atsisakyti</translation>
     </message>
     <message>
-        <location filename="../usbunmounter.cpp" line="332"/>
-        <location filename="../usbunmounter.cpp" line="334"/>
+        <location filename="../usbunmounter.cpp" line="322"/>
+        <location filename="../usbunmounter.cpp" line="324"/>
         <source>License</source>
         <translation>Licencija</translation>
     </message>
