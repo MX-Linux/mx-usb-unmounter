1cece9367024c361047f8fd0fa43c7ffbf29783e
Adrian <adrian@mxlinux.org>
2024-08-09 09:26:33 -0400

    Add message if already running

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
-    QString user = QProcessEnvironment::systemEnvironment().value("USER");
-    QLockFile lockfile("/var/lock/mx-usb-unmounter_" + user + ".lock");
+    const QString user = QProcessEnvironment::systemEnvironment().value("USER");
+    QLockFile lockfile(QString("/var/lock/mx-usb-unmounter_%1.lock").arg(user));
     if (!lockfile.tryLock()) {
+        qWarning("Another instance is already running.");
         return EXIT_FAILURE;
     }
 
